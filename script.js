a70e618e8a0a67e536304ca159ccefaa31d761c0
guylamdan00 <guylamdan00@gmail.com>
2021-08-19 17:18:27 +0300

    feat: Load more photos when scrolled to bottomed

--- script.js
+++ script.js
@@ -48,5 +48,13 @@ async function getPhotos() {
     }
 }
 
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    getPhotos();
+    console.log('Load more photos');
+}
+});
+
 // On Load
 getPhotos();
